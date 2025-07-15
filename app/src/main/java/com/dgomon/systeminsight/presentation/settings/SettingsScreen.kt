574a62a13e660495d02fa885898857583d29fea5
Dmitri Gomon <dgomon42@gmail.com>
2025-07-15 10:22:08 +0300

    Add Shizuku toggle to Settings screen

--- app/src/main/java/com/dgomon/systeminsight/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/dgomon/systeminsight/presentation/settings/SettingsScreen.kt
@@ -1,25 +1,26 @@
 package com.dgomon.systeminsight.presentation.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.dgomon.systeminsight.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
@@ -35,34 +36,35 @@ fun SettingsScreen(
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Column {
-            Button(
-                onClick = { settingsViewModel.requestPrivileges() },
-                enabled = !isConnected
-            ) {
-                Text(text = "Connect")
-            }
-
-            Button(
-                onClick = { settingsViewModel.releasePrivileges() },
-                enabled = isConnected
-            ) {
-                Text(text = "Disconnect")
-            }
-        }
-
-        val scrollState = rememberScrollState()
-
-        Text(
-            text = isConnected.toString(),
-            style = MaterialTheme.typography.bodyMedium,
+        Row(
             modifier = Modifier
-                .align(Alignment.BottomStart)
                 .fillMaxWidth()
-                .height(144.dp)
-                .verticalScroll(scrollState)
-                .padding(vertical = 2.dp)
-        )
-    }
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column (modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(R.string.use_shizuku_backend),
+                    style = MaterialTheme.typography.titleMedium
+                )
 
+                Text(
+                    text = stringResource(R.string.grant_shizuku_access),
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+
+            Switch(
+                checked = isConnected,
+                onCheckedChange = { checked ->
+                    if (checked) {
+                        settingsViewModel.requestPrivileges()
+                    } else {
+                        settingsViewModel.releasePrivileges()
+                    }
+                }
+            )
+        }
+    }
 }
