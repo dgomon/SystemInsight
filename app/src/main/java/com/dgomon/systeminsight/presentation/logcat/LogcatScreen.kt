75841c23d86184f9765a60f5bb1f3c3c79c41cc3
Dmitri Gomon <dgomon42@gmail.com>
2025-07-22 06:17:40 +0300

    Display an informative message when Logcat is empty

--- app/src/main/java/com/dgomon/systeminsight/presentation/logcat/LogcatScreen.kt
+++ app/src/main/java/com/dgomon/systeminsight/presentation/logcat/LogcatScreen.kt
@@ -3,22 +3,28 @@ package com.dgomon.systeminsight.presentation.logcat
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -32,12 +38,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
+import com.dgomon.systeminsight.R
 import com.dgomon.systeminsight.ui.common.HighlightedText
 import com.dgomon.systeminsight.ui.common.RequirePrivilegedConnection
 import kotlinx.coroutines.flow.collectLatest
@@ -93,80 +102,117 @@ fun LogcatScreen(
                 }
             }
 
-            LazyColumn(
-                state = listState,
-                modifier = modifier
-                    .fillMaxSize()
-            ) {
-                items(parsedLogEntries) { entry ->
-                    Column(
+            if (parsedLogEntries.isEmpty()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(24.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Info, // Or any relevant icon
+                        contentDescription = stringResource(R.string.no_logs),
+                        tint = MaterialTheme.colorScheme.primary,
                         modifier = Modifier
-                            .fillMaxHeight()
-                            .background(color = logLevelBackgroundColor(entry.logLevel))
-                            .height(IntrinsicSize.Min)
-                            .clickable(enabled = true, onClick = {
-                                logcatViewModel.onRowClicked(entry.rawLine)
-                            }),
-                        ) {
+                            .size(48.dp)
+                            .padding(bottom = 8.dp)
+                    )
 
-                        HorizontalDivider(
-                            thickness = DividerDefaults.Thickness,
-                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
-                        )
+                    Spacer(modifier = Modifier.height(12.dp))
 
-                        Row(
+                    Text(
+                        text = stringResource(R.string.no_logs_to_display),
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    Text(
+                        text = stringResource(R.string.press_the_play),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+            else {
+                LazyColumn(
+                    state = listState,
+                    modifier = modifier
+                        .fillMaxSize()
+                ) {
+                    items(parsedLogEntries) { entry ->
+                        Column(
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .height(IntrinsicSize.Min),
-                            verticalAlignment = Alignment.CenterVertically,
+                                .fillMaxHeight()
+                                .background(color = logLevelBackgroundColor(entry.logLevel))
+                                .height(IntrinsicSize.Min)
+                                .clickable(enabled = true, onClick = {
+                                    logcatViewModel.onRowClicked(entry.rawLine)
+                                }),
                         ) {
-                            Box(
+
+                            HorizontalDivider(
+                                thickness = DividerDefaults.Thickness,
+                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+                            )
+
+                            Row(
                                 modifier = Modifier
-                                    .fillMaxHeight()
-                                    .width(32.dp)  // Fixed width for logLevel
-                                    .padding(horizontal = 8.dp, vertical = 4.dp),
-                                contentAlignment = Alignment.Center
+                                    .fillMaxWidth()
+                                    .height(IntrinsicSize.Min),
+                                verticalAlignment = Alignment.CenterVertically,
                             ) {
-                                Text(
-                                    text = entry.logLevel,
-                                    style = MaterialTheme.typography.bodyMedium,
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .width(32.dp)  // Fixed width for logLevel
+                                        .padding(horizontal = 8.dp, vertical = 4.dp),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = entry.logLevel,
+                                        style = MaterialTheme.typography.bodyMedium,
+                                    )
+                                }
+
+                                VerticalDivider(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .width(1.dp),
+                                    thickness = DividerDefaults.Thickness,
+                                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+                                )
+
+                                HighlightedText(
+                                    text = entry.tag,
+                                    query = searchQuery,
+                                    style = MaterialTheme.typography.bodySmall,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis,
+                                    modifier = Modifier
+                                        .width(64.dp)
+                                        .padding(end = 8.dp)  // Optional, to balance padding
+                                )
+
+                                VerticalDivider(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                        .width(1.dp),
+                                    thickness = DividerDefaults.Thickness,
+                                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+                                )
+                                HighlightedText(
+                                    text = entry.message,
+                                    query = searchQuery,
+                                    style = MaterialTheme.typography.bodySmall,
+                                    modifier = Modifier
+                                        .weight(1f)  // Take all remaining space
+                                        .padding(end = 8.dp)  // Optional, to balance padding
                                 )
                             }
-
-                            VerticalDivider(
-                                modifier = Modifier
-                                    .fillMaxHeight()
-                                    .width(1.dp),
-                                thickness = DividerDefaults.Thickness,
-                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
-                            )
-
-                            HighlightedText(
-                                text = entry.tag,
-                                query = searchQuery,
-                                style = MaterialTheme.typography.bodySmall,
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis,
-                                modifier = Modifier
-                                    .width(64.dp)
-                                    .padding(end = 8.dp)  // Optional, to balance padding
-                            )
-
-                            VerticalDivider(
-                                modifier = Modifier
-                                    .fillMaxHeight()
-                                    .width(1.dp),
-                                thickness = DividerDefaults.Thickness,
-                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
-                            )
-                            HighlightedText(
-                                text = entry.message,
-                                query = searchQuery,
-                                style = MaterialTheme.typography.bodySmall,
-                                modifier = Modifier
-                                    .weight(1f)  // Take all remaining space
-                                    .padding(end = 8.dp)  // Optional, to balance padding
-                            )
                         }
                     }
                 }
